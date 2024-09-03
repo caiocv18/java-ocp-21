420915b350fc54d85af8b67bf5cdd4ff854258d9
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-09-02 23:12:53 -0300

    Add variable for constant value and print it

--- br/com/caiocv18/Variables.java
+++ br/com/caiocv18/Variables.java
@@ -3,11 +3,14 @@ package br.com.caiocv18;
 public class Variables{
     public static void main(String[] args) {
         final int[] numbers = new int[5];
+        final int ___ = 10;
 
         numbers[2] = 10;
-        //numbers = null;
+        //numbers = null; //DOES NOT COMPILE
         for(int i = 0; i<numbers.length; i++){
             System.out.println(numbers[i]);
         }
+
+        System.out.println(___);
     }
 }
