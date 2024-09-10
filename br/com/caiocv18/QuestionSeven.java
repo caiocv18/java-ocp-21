15b4f80e40600fee68340c7a5c0173aa9d290351
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-09-09 23:56:09 -0300

    Add QuestionSeven class with variables and print statement

---
+++ br/com/caiocv18/QuestionSeven.java
@@ -0,0 +1,12 @@
+package br.com.caiocv18;
+
+public class QuestionSeven {
+    private int numForks;
+    public static void main(String[] args) {
+       int numKnives;
+       System.out.print("""
+       "# forks = " + numForks +
+        " # knives = " + numKnives +
+       # cups = 0""");
+    }
+}
